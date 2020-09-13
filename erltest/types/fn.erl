c7b3133fa43baad2e6f28a2914c6300977560781
Leandro Ostera <leandro@ostera.io>
2020-09-14 01:12:08 +0200

    erlcomp: more examples for function signatures

--- erltest/types/fn.erl
+++ erltest/types/fn.erl
@@ -3,12 +3,22 @@
 
 -export_type([add/0]).
 -export_type([f/2]).
+-export_type([f_with_tuples/2]).
+-export_type([nested/0]).
 -export_type([predicate/1]).
+-export_type([r/1]).
 
 -type predicate(A) :: fun((A) -> bool()).
 
 -type add() :: fun((int(), int()) -> int()).
 
 -type f(A, B) :: fun((A, B) -> bool()).
+
+-type nested() :: fun((unit(), fun((int()) -> bool())) -> string()).
+
+-type f_with_tuples(A, B) :: fun(({A, B}, unit()) -> bool()).
+
+-type r(A) :: #{ f => fun((unit()) -> {A, int()})
+               }.
 
 
--- erltest/types/fn.ml
+++ erltest/types/fn.ml
@@ -4,3 +4,11 @@ type 'a predicate = 'a -> bool
 type add = int -> int -> int
 
 type ('a, 'b) f = 'a -> 'b -> bool
+
+type nested = unit -> (int -> bool) -> string
+
+type ('a, 'b) f_with_tuples = ('a * 'b) -> unit -> bool
+
+type 'a r = {
+  f: unit -> 'a * int
+}
