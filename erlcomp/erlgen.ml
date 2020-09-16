88a2b056b4768f7dbff408609f931cdfba36c69a
Leandro Ostera <leandro@ostera.io>
2020-09-16 22:10:23 +0200

    erlcomp: less output, more useful

--- erlcomp/erlgen.ml
+++ erlcomp/erlgen.ml
@@ -2,10 +2,14 @@ open Compile_common
 
 let generate_one erlmod =
   let open Erlast in
+  let _ = print_string ("Compiling " ^ erlmod.file_name ^ "\t" ) in
   let erlfile = erlmod.file_name in
   let oc = open_out_bin erlfile in
   Misc.try_finally
-    ~always:(fun () -> close_out oc)
+    ~always:(fun () ->
+      print_string "OK\n";
+      close_out oc
+    )
     ~exceptionally:(fun () -> Misc.remove_file erlfile)
     (fun () ->
         let f = Format.formatter_of_out_channel oc in
@@ -15,7 +19,6 @@ let generate_one erlmod =
 
 let generate_sources file_info (typedtree, _mod_coercion) signature =
   let erlmods = Erlconv.from_typedtree ~name:file_info.module_name typedtree signature in
-  let _ = print_string ("Generating " ^ (string_of_int (List.length erlmods)) ^ " sources...\n" ) in
   erlmods
   |> List.filter (fun erlmod -> Erlast.(erlmod.exports) <> [] )
   |> List.iter generate_one
