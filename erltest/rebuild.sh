46795d209ead383db52c6915c4a6e6a5735cd5f5
Leandro Ostera <leandro@ostera.io>
2020-09-19 19:10:06 +0200

    erltest: start running erlc with every rebuild to make sure things build on erlang too

--- erltest/rebuild.sh
+++ erltest/rebuild.sh
@@ -6,5 +6,6 @@ for d in expressions ffi functions modules types otp processes; do
   pushd $d;
     rm -f ./*.cm* ./*.erl ./*.beam
     ${CARAMELC} -c $(${CARAMELC} -depend -sort *.mli *.ml)
+    erlc *.erl || true
   popd;
 done
