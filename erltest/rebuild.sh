9a9fc6b2049ac3b29a47148041aa12a769cf4ca9
Leandro Ostera <leandro@ostera.io>
2020-09-19 18:06:14 +0200

    erltest: cleaner rebuild script

--- erltest/rebuild.sh
+++ erltest/rebuild.sh
@@ -1,33 +1,10 @@
 #!/bin/bash -e
 
-CARAMELC="../../caramelc -c"
+CARAMELC="../../caramelc"
 
-rm -f ./**/*.cm* ./**/*.erl ./**/*.beam
-
-pushd ./expressions;
-  ${CARAMELC} *.ml
-popd;
-
-pushd ./ffi;
-  ${CARAMELC} *.ml
-popd;
-
-pushd ./functions;
-  ${CARAMELC} io.ml *.mli *.ml
-popd
-
-pushd ./modules;
-  ${CARAMELC} *.mli *.ml
-popd
-
-pushd ./types;
-  ${CARAMELC} *.mli record.ml type_args.ml *.ml
-popd
-
-pushd ./otp;
-  ${CARAMELC} timer.ml erlang.ml gen_server.ml *.ml
-popd
-
-pushd ./processes;
-  ${CARAMELC} io.ml erlang.ml process.ml *.ml
-popd
+for d in expressions ffi functions modules types otp processes; do
+  pushd $d;
+    rm -f ./*.cm* ./*.erl ./*.beam
+    ${CARAMELC} -c $(${CARAMELC} -depend -sort *.mli *.ml)
+  popd;
+done
