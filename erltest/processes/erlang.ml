2575ca5992b56e4c1e7531561e85ffa94c4968d3
Leandro Ostera <leandro@ostera.io>
2020-09-22 23:54:05 +0200

    erltest: some removals and some proc registry prototypes

--- erltest/processes/erlang.ml
+++ erltest/processes/erlang.ml
@@ -5,3 +5,13 @@ external spawn : (unit -> 'a) -> 'message process = ""
 external send : 'message process -> 'message -> unit = ""
 
 external self : unit -> 'message process = ""
+
+external register : 'a -> 'message process -> bool = ""
+
+external whereis : 'a -> 'message process = ""
+
+external is_atom : 'a -> bool = ""
+
+external is_pid : 'a -> bool = ""
+
+external is_port : 'a -> bool = ""
---
+++ erltest/processes/proc_registry_2.ml
@@ -0,0 +1,17 @@
+type name = ..
+type registry = name list
+
+let registry: registry ref = ref []
+
+let where_is f =
+	 let rec find' ns = match ns with
+	 | [] -> None
+	 | n :: ns -> match (f n) with | Some t -> Some t | None -> find' ns
+	 in find' (registry.contents)
+
+let register f n pid =
+	match (find f) with
+	| Some _ -> Error "process already registered"
+	| None ->
+		registry := n :: registry.contents;
+		Ok ()
