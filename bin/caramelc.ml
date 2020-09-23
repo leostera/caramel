2539a49f8d2c805ac063a09291fac607c6084ba4
Leandro Ostera <leandro@ostera.io>
2020-09-23 19:56:30 +0200

    caramelc: new cli with both compile and sort-dep commands

--- bin/caramelc.ml
+++ bin/caramelc.ml
@@ -1,16 +1,78 @@
-let run () = ()
+open Cmdliner
 
-let info name =
-  Info.make ~name ~doc:"Caramel compiler"
-    ~description:
-      "Caramel is a compiler from OCaml to Erlang."
+module Compile = struct
+  let name = "compile"
 
-let command_main = (Cmdliner.Term.(pure run), info "caramelc")
+  let doc =
+    "Compile OCaml to idiomatic Erlang, OCaml to Core Erlang, or Erlang to a \
+     Binary."
 
-let compile = (Cmdliner.Term.(pure run), info "compile")
+  let description =
+    {| The Caramel compiler can take as input OCaml and
+  Erlang files, and will be able to compile them to either Erlang or Core
+  Erlang sources, or a binary.
+
+  OCaml inputs (.ml and .mli files) will be compiled to .erl and .core files
+  Erlang inputs (.erl files) will be compiled to an .exe binary
+  |}
+
+  let info = Info.make ~name ~doc ~description
+
+  let run sources = Caramel_compiler.Compiler.compile { sources }
+
+  let args =
+    let sources = Arg.(non_empty & pos_all file [] & info [] ~docv:"SOURCES") in
+    sources
+
+  let cmd = (Term.(pure run $ args), info)
+end
+
+module Sort_deps = struct
+  let name = "sort-deps"
+
+  let doc = "Sort OCaml files by their dependencies on each other."
+
+  let description = {||}
+
+  let info = Info.make ~name ~doc ~description
+
+  let run sources = Caramel_compiler.Dependency_sorter.print_sorted_files sources
+
+  let args =
+    let sources = Arg.(non_empty & pos_all file [] & info [] ~docv:"SOURCES") in
+    sources
+
+  let cmd = (Term.(pure run $ args), info)
+end
+
+(*
+module Typecheck = struct
+  let name = "check"
+  let doc = "Typecheck Erlang and Core Erlang sources"
+  let description = {| The Caramel compiler can take as input Erlang and
+  Core Erlang files and it will type check them.
+  |}
+  let info = Info.make ~name ~doc ~description
+
+  let run _sources = ()
+
+  let args =
+    let sources = Arg.(non_empty & pos_all file [] & info [] ~docv:"SOURCES") in
+    sources
+
+  let cmd = Term.(pure run), info
+end
+*)
+
+module Help = struct
+  let info name =
+    let doc = "Caramel compiler" in
+    let description = "Caramel is a compiler from OCaml to Erlang." in
+    Info.make ~name ~doc ~description
+
+  let cmd = (Term.(ret (const (`Help (`Pager, None)))), info "caramelc")
+end
 
 let _ =
-  Cmdliner.Term.eval_choice command_main
-    [
-      compile
-    ]
+  let cmds = [ Compile.cmd; Sort_deps.cmd ] in
+  Term.(exit @@ eval_choice Help.cmd cmds)
--- bin/info.ml
+++ bin/info.ml
@@ -3,19 +3,20 @@ open Cmdliner
 
 let make ~name ~doc ~description =
   let man =
-    [ `S "DESCRIPTION"
-    ; `P description
-    ; `S "SEE ALSO"
-    ; `P "ocaml(1)"
-    ; `S "AUTHORS"
-    ; `P "Leandro Ostera."
-    ; `S "LICENSE"
-    ; `P "Copyright (C) 2020."
-    ; `P
-        "caramel is free software, you can redistribute it and/or modify it under \
-         the terms of the GNU Lesser General Public License as published by the Free \
-         Software Foundation, with linking exception; either version 2.1 of the License, \
-         or (at your option) any later version."
+    [
+      `S "DESCRIPTION";
+      `P description;
+      `S "SEE ALSO";
+      `P "ocaml(1) erlang";
+      `S "AUTHORS";
+      `P "Leandro Ostera.";
+      `S "LICENSE";
+      `P "Copyright (C) 2020.";
+      `P
+        "caramel is free software, you can redistribute it and/or modify it \
+         under the terms of the GNU Lesser General Public License as published \
+         by the Free Software Foundation, with linking exception; either \
+         version 2.1 of the License, or (at your option) any later version.";
     ]
   in
   let version =
