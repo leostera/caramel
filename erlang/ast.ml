c7967e20861a03f07f683142c71bad6cb89e05cb
Leandro Ostera <leandro@ostera.io>
2020-09-26 17:42:31 +0200

    erlang: simplify ast

--- erlang/ast.ml
+++ erlang/ast.ml
@@ -1,21 +1,19 @@
 open Sexplib.Std
 
-type atom = string [@@deriving sexp]
-
-and var_name = string [@@deriving sexp]
+type atom = Atom of string [@@deriving sexp]
 
 and guard = unit [@@deriving sexp]
 
 and name =
-  | Var_name of atom
+  | Var_name of string
   | Atom_name of atom
-  | Macro_name of atom
   | Qualified_name of { n_mod : atom; n_name : atom }
 [@@deriving sexp]
 
 and map_field = { mf_name : atom; mf_value : expr } [@@deriving sexp]
 
-and case_branch = { cb_pattern : pattern; cb_expr : expr } [@@deriving sexp]
+and case = { c_lhs : pattern list; c_guard : guard option; c_rhs : expr }
+[@@deriving sexp]
 
 and let_binding = { lb_lhs : pattern; lb_rhs : expr } [@@deriving sexp]
 
@@ -24,31 +22,30 @@ and literal =
   | Lit_char of string
   | Lit_binary of string
   | Lit_float of string
-  | Lit_atom of string
+  | Lit_atom of atom
 [@@deriving sexp]
 
-and recv = { rcv_cases : case_branch list; rcv_after : case_branch option }
-[@@deriving sexp]
+and recv = { rcv_cases : case list; rcv_after : case option } [@@deriving sexp]
 
 and expr =
   | Expr_let of let_binding * expr
   | Expr_name of name
   | Expr_literal of literal
-  (* NOTE: function references will be resolved at print time for now :( *)
-  | Expr_fun_ref of atom
+  | Expr_fun_ref of { fref_name : atom; fref_arity : int }
   | Expr_apply of fun_apply
   | Expr_recv of recv
   | Expr_map of map_field list
   | Expr_list of expr list
+  | Expr_nil
   | Expr_cons of expr list * expr
-  | Expr_case of expr * case_branch list
+  | Expr_case of expr * case list
   | Expr_tuple of expr list
-  | Expr_fun of fun_decl
+  | Expr_fun of case list
 [@@deriving sexp]
 
 and pattern =
   | Pattern_ignore
-  | Pattern_binding of atom
+  | Pattern_binding of name
   | Pattern_tuple of pattern list
   | Pattern_list of pattern list
   | Pattern_cons of pattern list * pattern
@@ -58,18 +55,10 @@ and pattern =
 
 and fun_apply = { fa_name : expr; fa_args : expr list } [@@deriving sexp]
 
-and fun_case = {
-  fc_name : atom;
-  fc_lhs : pattern list;
-  fc_guards : guard list;
-  fc_rhs : expr;
-}
-[@@deriving sexp]
-
 and fun_decl = {
   fd_name : atom;
   fd_arity : int;
-  fd_cases : fun_case list;
+  fd_cases : case list;
   fd_spec : type_kind option;
 }
 [@@deriving sexp]
@@ -89,12 +78,12 @@ and variant_constructor = Constructor of type_constr | Extension of type_kind
 and type_constr = { tc_name : name; tc_args : type_kind list } [@@deriving sexp]
 
 and type_kind =
-  | Type_function of type_kind list
+  | Type_function of { tyfun_args : type_kind list; tyfun_return : type_kind }
   | Type_constr of type_constr
-  | Type_variable of atom
+  | Type_variable of name
   | Type_tuple of type_kind list
-  | Type_record of { fields : record_field list }
-  | Type_variant of { constructors : variant_constructor list }
+  | Type_record of { tyrec_fields : record_field list }
+  | Type_variant of { tyvar_constructors : variant_constructor list }
 [@@deriving sexp]
 
 and type_visibility = Opaque | Visible [@@deriving sexp]
@@ -103,7 +92,7 @@ and type_decl = {
   typ_kind : type_kind;
   typ_visibility : type_visibility;
   typ_name : atom;
-  typ_params : var_name list;
+  typ_params : name list;
 }
 [@@deriving sexp]
 
@@ -125,11 +114,12 @@ and module_item =
   | Function_decl of fun_decl
 [@@deriving sexp]
 
+and structure = module_item list [@@deriving sexp]
+
 and t = {
   file_name : string;
   behaviours : atom list;
   module_name : atom;
-  ocaml_name : atom;
   attributes : attribute list;
   exports : export list;
   types : type_decl list;
@@ -141,162 +131,3 @@ and t = {
     See:
       http://erlang.org/doc/reference_manual/modules.html
  *)
-
-let empty =
-  {
-    file_name = "empty.erl";
-    behaviours = [];
-    module_name = "empty";
-    ocaml_name = "empty";
-    attributes = [];
-    exports = [];
-    types = [];
-    functions = [];
-  }
-
-let rec item_list_to_module items acc =
-  match items with
-  | [] -> acc
-  | x :: xs ->
-      item_list_to_module xs
-        ( match x with
-        | Module_attribute
-            {
-              atr_name = "module";
-              atr_value = Expr_literal (Lit_atom module_name);
-            } ->
-            {
-              acc with
-              module_name;
-              file_name = module_name ^ ".erl";
-              ocaml_name = String.capitalize_ascii module_name;
-            }
-        | Module_attribute
-            {
-              atr_name = "behavior";
-              atr_value = Expr_literal (Lit_atom behavior);
-            }
-        | Module_attribute
-            {
-              atr_name = "behaviour";
-              atr_value = Expr_literal (Lit_atom behavior);
-            } ->
-            { acc with behaviours = behavior :: acc.behaviours }
-        | Module_attribute { atr_name = "export"; atr_value = Expr_list attrs }
-          ->
-            {
-              acc with
-              exports =
-                ( attrs
-                |> List.filter_map (function
-                     | Expr_tuple
-                         [
-                           Expr_literal (Lit_atom exp_name);
-                           Expr_literal (Lit_integer exp_arity);
-                         ] ->
-                         Some
-                           {
-                             exp_type = Export_function;
-                             exp_name;
-                             exp_arity = int_of_string exp_arity;
-                           }
-                     | _ -> None) )
-                @ acc.exports;
-            }
-        | Module_attribute
-            { atr_name = "export_type"; atr_value = Expr_list attrs } ->
-            {
-              acc with
-              exports =
-                ( attrs
-                |> List.filter_map (function
-                     | Expr_tuple
-                         [
-                           Expr_literal (Lit_atom exp_name);
-                           Expr_literal (Lit_integer exp_arity);
-                         ] ->
-                         Some
-                           {
-                             exp_type = Export_type;
-                             exp_name;
-                             exp_arity = int_of_string exp_arity;
-                           }
-                     | _ -> None) )
-                @ acc.exports;
-            }
-        | Module_attribute atr ->
-            { acc with attributes = atr :: acc.attributes }
-        | Type_decl td -> { acc with types = td :: acc.types }
-        | Function_decl fd -> { acc with functions = fd :: acc.functions } )
-
-let of_module_items items =
-  match items with
-  | [] -> Error `Module_item_list_was_empty
-  | [
-   Module_attribute
-     { atr_name = "module"; atr_value = Expr_literal (Lit_atom module_name) };
-  ] ->
-      Ok
-        {
-          empty with
-          module_name;
-          file_name = module_name ^ ".erl";
-          ocaml_name = String.capitalize_ascii module_name;
-        }
-  | [ _ ] -> Error `Single_module_item_was_not_a_module_name
-  | xs -> Ok (item_list_to_module xs empty)
-
-let make ~name ~ocaml_name ~exports ~types ~functions ~attributes =
-  {
-    file_name = name ^ ".erl";
-    behaviours = [];
-    module_name = name;
-    ocaml_name;
-    attributes;
-    exports;
-    types;
-    functions;
-  }
-
-let make_fn_export exp_name exp_arity =
-  { exp_type = Export_function; exp_name; exp_arity }
-
-let make_type_export exp_name exp_arity =
-  { exp_type = Export_type; exp_name; exp_arity }
-
-let make_named_type typ_name typ_params typ_kind typ_visibility =
-  let used_params : atom list =
-    let rec collect_params acc k =
-      let flat_args =
-        match k with
-        | Type_function args -> List.concat_map (collect_params []) args
-        | Type_constr { tc_args; _ } ->
-            tc_args |> List.concat_map (collect_params [])
-        | Type_variable atom -> [ atom ]
-        | Type_tuple args -> List.concat_map (collect_params []) args
-        | Type_record { fields } ->
-            fields
-            |> List.map (fun { rf_type; _ } -> rf_type)
-            |> List.concat_map (collect_params [])
-        | Type_variant { constructors } ->
-            constructors
-            |> List.concat_map (function
-                 | Constructor { tc_args; _ } ->
-                     List.concat_map (collect_params []) tc_args
-                 | Extension ext -> collect_params [] ext)
-      in
-      [ flat_args; acc ] |> List.concat
-    in
-    collect_params [] typ_kind
-  in
-  let typ_params =
-    typ_params
-    |> List.map (fun p ->
-           if List.exists (fun up -> up = p) used_params then p else "_" ^ p)
-  in
-  { typ_name; typ_params; typ_kind; typ_visibility }
-
-let type_any = Type_constr { tc_name = Atom_name "any"; tc_args = [] }
-
-let find_fun_by_name ~module_ name =
-  module_.functions |> List.find_opt (fun { fd_name; _ } -> fd_name = name)
--- erlang/ast.mli
+++ erlang/ast.mli
@@ -1,16 +1,15 @@
-type atom = string
+type atom = Atom of string
 
 and guard = unit
 
 and name =
-  | Var_name of atom
+  | Var_name of string
   | Atom_name of atom
-  | Macro_name of atom
   | Qualified_name of { n_mod : atom; n_name : atom }
 
 and map_field = { mf_name : atom; mf_value : expr }
 
-and case_branch = { cb_pattern : pattern; cb_expr : expr }
+and case = { c_lhs : pattern list; c_guard : guard option; c_rhs : expr }
 
 and let_binding = { lb_lhs : pattern; lb_rhs : expr }
 
@@ -19,27 +18,28 @@ and literal =
   | Lit_char of string
   | Lit_binary of string
   | Lit_float of string
-  | Lit_atom of string
+  | Lit_atom of atom
 
-and recv = { rcv_cases : case_branch list; rcv_after : case_branch option }
+and recv = { rcv_cases : case list; rcv_after : case option }
 
 and expr =
   | Expr_let of let_binding * expr
   | Expr_name of name
   | Expr_literal of literal
-  | Expr_fun_ref of atom
+  | Expr_fun_ref of { fref_name : atom; fref_arity : int }
   | Expr_apply of fun_apply
   | Expr_recv of recv
   | Expr_map of map_field list
   | Expr_list of expr list
+  | Expr_nil
   | Expr_cons of expr list * expr
-  | Expr_case of expr * case_branch list
+  | Expr_case of expr * case list
   | Expr_tuple of expr list
-  | Expr_fun of fun_decl
+  | Expr_fun of case list
 
 and pattern =
   | Pattern_ignore
-  | Pattern_binding of atom
+  | Pattern_binding of name
   | Pattern_tuple of pattern list
   | Pattern_list of pattern list
   | Pattern_cons of pattern list * pattern
@@ -48,17 +48,10 @@ and pattern =
 
 and fun_apply = { fa_name : expr; fa_args : expr list }
 
-and fun_case = {
-  fc_name : atom;
-  fc_lhs : pattern list;
-  fc_guards : guard list;
-  fc_rhs : expr;
-}
-
 and fun_decl = {
   fd_name : atom;
   fd_arity : int;
-  fd_cases : fun_case list;
+  fd_cases : case list;
   fd_spec : type_kind option;
 }
 
@@ -69,12 +62,12 @@ and variant_constructor = Constructor of type_constr | Extension of type_kind
 and type_constr = { tc_name : name; tc_args : type_kind list }
 
 and type_kind =
-  | Type_function of type_kind list
+  | Type_function of { tyfun_args : type_kind list; tyfun_return : type_kind }
   | Type_constr of type_constr
-  | Type_variable of atom
+  | Type_variable of name
   | Type_tuple of type_kind list
-  | Type_record of { fields : record_field list }
-  | Type_variant of { constructors : variant_constructor list }
+  | Type_record of { tyrec_fields : record_field list }
+  | Type_variant of { tyvar_constructors : variant_constructor list }
 
 and type_visibility = Opaque | Visible
 
@@ -82,7 +75,7 @@ and type_decl = {
   typ_kind : type_kind;
   typ_visibility : type_visibility;
   typ_name : atom;
-  typ_params : atom list;
+  typ_params : name list;
 }
 
 (** An exported symbol in an Erlang module. This could be a function or a type.
@@ -101,11 +94,12 @@ and module_item =
   | Type_decl of type_decl
   | Function_decl of fun_decl
 
+and structure = module_item list
+
 and t = {
   file_name : string;
   behaviours : atom list;
   module_name : atom;
-  ocaml_name : atom;
   attributes : attribute list;
   exports : export list;
   types : type_decl list;
@@ -113,32 +107,3 @@ and t = {
 }
 
 val sexp_of_t : t -> Sexplib.Sexp.t
-
-val empty : t
-
-val of_module_items :
-  module_item list ->
-  ( t,
-    [> `Module_item_list_was_empty | `Single_module_item_was_not_a_module_name ]
-  )
-  result
-
-val make :
-  name:string ->
-  ocaml_name:string ->
-  exports:export list ->
-  types:type_decl list ->
-  functions:fun_decl list ->
-  attributes:attribute list ->
-  t
-
-val make_fn_export : atom -> int -> export
-
-val make_type_export : atom -> int -> export
-
-val make_named_type :
-  atom -> atom list -> type_kind -> type_visibility -> type_decl
-
-val type_any : type_kind
-
-val find_fun_by_name : module_:t -> string -> fun_decl option
