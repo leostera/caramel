22512550fd901e9d1c71b5cd044e363e9bb19e65
Leandro Ostera <leandro@ostera.io>
2020-09-26 23:08:12 +0200

    runtime: bootstrap runtime with notes

---
+++ runtime/process.ml
@@ -0,0 +1,22 @@
+type after_time = Infinity | Bounded of int
+
+type 'm recv = timeout:after_time -> 'm option
+
+external __caramel_recv : 'a -> 'm option = ""
+
+let recv ~timeout =
+  let f = __caramel_recv in
+  match timeout with Infinity -> f `Infinity | Bounded t -> f t
+
+let spawn : ('m Erlang.pid -> 'm recv -> 'a) -> 'm Erlang.pid =
+ fun f ->
+  Erlang.spawn (fun () ->
+      let pid : 'm Erlang.pid = Erlang.self () in
+      f pid recv)
+
+let send : 'm Erlang.pid -> 'm -> unit = fun proc msg -> Erlang.send proc msg
+
+let contramap : ('b -> 'a) -> 'a Erlang.pid -> 'b Erlang.pid =
+ fun f pid ->
+  spawn (fun _self recv ->
+      match recv ~timeout:Infinity with Some a -> send pid (f a) | None -> ())
