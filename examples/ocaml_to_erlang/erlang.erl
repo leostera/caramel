685067831cac0826906c561024b7cc48718d483c
Leandro Ostera <leandro@ostera.io>
2020-10-06 20:04:16 +0200

    examples: remove redundant files

--- examples/ocaml_to_erlang/erlang.erl
+++
@@ -1,8 +0,0 @@
-% Source code generated with Caramel.
--module(erlang).
--export_type([pid/1]).
-
-
--opaque pid(M) :: reference().
-
-
--- examples/ocaml_to_erlang/io.ml
+++
@@ -1,1 +0,0 @@
-external format : string -> 'a list -> unit = ""
