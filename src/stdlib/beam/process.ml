47829deeb562f5727eb06ea19e7bed9068a83d6e
Leandro Ostera <leandro@ostera.io>
2020-10-22 12:20:15 +0200

    stdlib: fix Process.spawn in tests

--- src/stdlib/beam/process.ml
+++ src/stdlib/beam/process.ml
@@ -8,7 +8,7 @@ let recv ~timeout =
   let f = __caramel_recv in
   match timeout with Infinity -> f `Infinity | Bounded t -> f t
 
-let spawn : ('m Erlang.pid -> 'm recv -> 'a) -> 'm Erlang.pid =
+let make : ('m Erlang.pid -> 'm recv -> 'a) -> 'm Erlang.pid =
  fun f ->
   Erlang.spawn (fun () ->
       let pid : 'm Erlang.pid = Erlang.self () in
@@ -18,5 +18,5 @@ let send : 'm Erlang.pid -> 'm -> unit = fun proc msg -> Erlang.send proc msg
 
 let contramap : ('b -> 'a) -> 'a Erlang.pid -> 'b Erlang.pid =
  fun f pid ->
-  spawn (fun _self recv ->
+  make (fun _self recv ->
       match recv ~timeout:Infinity with Some a -> send pid (f a) | None -> ())
