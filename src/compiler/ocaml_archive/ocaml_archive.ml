4771f8f09ccc1a69a1b386a6399736dddf57abd9
Leandro Ostera <leandro@ostera.io>
2020-11-05 16:43:08 +0100

    compiler: remove unused obj generation from archival

--- src/compiler/ocaml_archive/ocaml_archive.ml
+++ src/compiler/ocaml_archive/ocaml_archive.ml
@@ -2,31 +2,6 @@ open Compile_common
 
 let tool_name = "caramelc:ocaml-archive"
 
-let to_bytecode i lambda =
-  lambda
-  |> Profile.(record ~accumulate:true generate)
-       (fun Lambda.{ code = lambda; required_globals; _ } ->
-         let simp_lambda = lambda |> Simplif.simplify_lambda in
-         let bytecode =
-           simp_lambda |> Bytegen.compile_implementation i.module_name
-         in
-         (bytecode, required_globals))
-
-let to_lambda i (typedtree, coercion) =
-  (typedtree, coercion)
-  |> Profile.(record transl) (Translmod.transl_implementation i.module_name)
-
-let emit_bytecode i (bytecode, required_globals) =
-  let cmofile = i.output_prefix ^ ".cmo" in
-  let oc = open_out_bin cmofile in
-  Misc.try_finally
-    ~always:(fun () -> close_out oc)
-    ~exceptionally:(fun () -> Misc.remove_file cmofile)
-    (fun () ->
-      bytecode
-      |> Profile.(record ~accumulate:true generate)
-           (Emitcode.to_file oc i.module_name cmofile ~required_globals))
-
 let read_signature info =
   let module_name = info.module_name in
   let cmi_file = module_name ^ ".cmi" in
@@ -37,10 +12,7 @@ let read_signature info =
   with Not_found -> None
 
 let archive ~source_file ~output_prefix ~opts:_ =
-  let backend info typed =
-    let lambda = to_lambda info typed in
-    let bytecode = to_bytecode info lambda in
-    emit_bytecode info bytecode;
+  let backend _info _typed =
     Bytelibrarian.create_archive
       (Compenv.get_objfiles ~with_ocamlparam:false)
       (output_prefix ^ ".cma")
