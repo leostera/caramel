2662c88e4830dcd7735f897fd89fa7d289cb917e
Leandro Ostera <leandro@ostera.io>
2020-11-05 16:39:48 +0100

    typing: remove unused bytecode object generation

--- src/typing/erlang_as_ocaml.ml
+++ src/typing/erlang_as_ocaml.ml
@@ -2,31 +2,6 @@ open Compile_common
 
 let tool_name = "caramelc:typing:erlang_as_ocaml"
 
-let to_bytecode i lambda =
-  lambda
-  |> Profile.(record ~accumulate:true generate)
-       (fun Lambda.{ code = lambda; required_globals; _ } ->
-         let simp_lambda = lambda |> Simplif.simplify_lambda in
-         let bytecode =
-           simp_lambda |> Bytegen.compile_implementation i.module_name
-         in
-         (bytecode, required_globals))
-
-let to_lambda i (typedtree, coercion) =
-  (typedtree, coercion)
-  |> Profile.(record transl) (Translmod.transl_implementation i.module_name)
-
-let emit_bytecode i (bytecode, required_globals) =
-  let cmofile = i.output_prefix ^ ".cmo" in
-  let oc = open_out_bin cmofile in
-  Misc.try_finally
-    ~always:(fun () -> close_out oc)
-    ~exceptionally:(fun () -> Misc.remove_file cmofile)
-    (fun () ->
-      bytecode
-      |> Profile.(record ~accumulate:true generate)
-           (Emitcode.to_file oc i.module_name cmofile ~required_globals))
-
 let parse ~source_file ~dump_ast =
   let erlang_ast =
     match Erlang.Parse.from_file source_file with
@@ -50,14 +25,10 @@ let check ~source_file ~output_prefix ~dump_ast =
     ~dump_ext:"cmo"
   @@ fun i ->
   let parsetree = parse ~source_file ~dump_ast in
-  let typedtree =
+  let (typed, _coercion) =
     parsetree
     |> Profile.(record typing)
          (Typemod.type_implementation i.source_file i.output_prefix
             i.module_name i.env)
   in
-  let lambda = to_lambda i typedtree in
-  let bytecode = to_bytecode i lambda in
-  emit_bytecode i bytecode;
-  let typed, _coerdion = typedtree in
   `Structure typed
