53d66c7e7343cb9a75dfbf910e7684f84be3389f
Leandro Ostera <leandro@ostera.io>
2020-11-05 19:10:16 +0100

    compiler: remove unused read_signature from ocaml archives

--- src/compiler/ocaml_archive/ocaml_archive.ml
+++ src/compiler/ocaml_archive/ocaml_archive.ml
@@ -2,15 +2,6 @@ open Compile_common
 
 let tool_name = "caramelc:ocaml-archive"
 
-let read_signature info =
-  let module_name = info.module_name in
-  let cmi_file = module_name ^ ".cmi" in
-  try
-    let intf_file = Load_path.find_uncap cmi_file in
-    let sign = Env.read_signature module_name intf_file in
-    Some sign
-  with Not_found -> None
-
 let archive ~source_file ~output_prefix ~opts:_ =
   let backend _info _typed =
     Bytelibrarian.create_archive
